c4ee5e90b575a0f6980fb6ce057965d93dc94ef4
Robert Middleswarth <robert@middleswarth.net>
2012-04-03 00:25:18 -0400

    Make the refresh rate configurable.  It defaults to 60 seconds.  This will only work in 5.7 or greater so no back porting this to 5.6

--- api/App.php
+++ api/App.php
@@ -1,11 +1,53 @@
 <?php
+if(class_exists('Extension_PluginSetup')):
+    class Cerb5blogAutorefreshNotifications_Setup extends Extension_PluginSetup {
+        const POINT = 'cerb5blog.autorefresh.notifications.setup';
+
+        function render() {
+            $tpl = DevblocksPlatform::getTemplateService();
+
+            $params = array(
+                'refreshrate' => DevblocksPlatform::getPluginSetting('cerb5blog.autorefresh.notifications','refreshrate','60'),
+            );
+            $tpl->assign('params', $params);
+            
+            $tpl->display('devblocks:cerb5blog.autorefresh.notifications::setup.tpl');
+        }
+
+        function save(&$errors) {
+            try {
+                @$refreshrate = DevblocksPlatform::importGPC($_REQUEST['refreshrate'],'string','60');
+
+                if(empty($refreshrate))
+                    throw new Exception("Must set a refresh rate.");
+                
+                if(($refreshrate < 5) || ($refreshrate > 1000))
+                    throw new Exception("Range must be between 5 and 1000 seconds.");
+                
+                DevblocksPlatform::setPluginSetting('cerb5blog.autorefresh.notifications','refreshrate',$refreshrate);
+
+                return true;
+
+            } catch (Exception $e) {
+                $errors[] = $e->getMessage();
+                return false;
+            }
+        }
+    };
+endif;
+
 if (class_exists('Extension_AppPreBodyRenderer',true)):
-	class Cerb5blogAutorefreshNotificationsPreBodyRenderer extends Extension_AppPreBodyRenderer {
-		function render() { 
-                    $tpl = DevblocksPlatform::getTemplateService();
-                    $tpl->display('devblocks:cerb5blog.autorefresh.notifications::header.tpl');
-		}
-	};
+    class Cerb5blogAutorefreshNotificationsPreBodyRenderer extends Extension_AppPreBodyRenderer {
+        function render() { 
+            $tpl = DevblocksPlatform::getTemplateService();
+            $refreshrate = intval(DevblocksPlatform::getPluginSetting('cerb5blog.autorefresh.notifications','refreshrate','60'));
+            if(($refreshrate < 5) || ($refreshrate > 1000)) {
+                $refreshrate = 60;
+            }
+            $tpl->assign('refreshrate', $refreshrate);
+            $tpl->display('devblocks:cerb5blog.autorefresh.notifications::header.tpl');
+        }
+    };
 endif;
 
 if (class_exists('DevblocksControllerExtension',true)):
